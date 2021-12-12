95e46466622670ebe35aed550711748a7cb37b7d
Christoph Ladurner <christoph.ladurner@tugraz.at>
2021-12-12 11:53:01 +0100

    bugfix: convert loaded motorcycle files to corresponding objects and apply prettier

--- src/commands/IntersectionCache.ts
+++ src/commands/IntersectionCache.ts
@@ -1,8 +1,8 @@
-import type { Arguments, CommandBuilder } from 'yargs';
-import * as fs from 'fs';
+import type { Arguments, CommandBuilder } from "yargs";
+import * as fs from "fs";
 import * as geom from "geometric";
 import * as mc from "motorcycleGraph";
-const { PerformanceObserver, performance } = require('perf_hooks');
+const { PerformanceObserver, performance } = require("perf_hooks");
 
 type Options = {
   data: string;
@@ -10,22 +10,20 @@ type Options = {
 };
 
 function load(filename) {
-  const rawdata = fs.readFileSync(filename, 'utf-8');
+  const rawdata = fs.readFileSync(filename, "utf-8");
   return JSON.parse(rawdata);
 }
 
-export const command: string = 'intersection-cache';
-export const description: string = 'intersection-cache';
+export const command: string = "intersection-cache";
+export const description: string = "intersection-cache";
 
 // according to the documentation there should following as the type:
 // CommandBuilder<Options, Options> but the compiler could not handle it.
 export const builder: any = (yargs) =>
-  yargs
-    .options({
-      data: { type: 'string' },
-      output: { type: "string", demandOption: false }
-    });
-
+  yargs.options({
+    data: { type: "string" },
+    output: { type: "string", demandOption: false },
+  });
 
 export const handler = (argv: Arguments<Options>): void => {
   if (!fs.existsSync(argv.data)) {
@@ -33,22 +31,22 @@ export const handler = (argv: Arguments<Options>): void => {
     return;
   }
 
-  const obj = load(argv.data)
+  const obj = load(argv.data);
 
-  const polygon = obj["polygon"].map(o => [o.x, o.y]);
-  const points = polygon.map(o => geom.Point.fromArray(o));
+  const polygon = obj["polygon"].map((o) => [o.x, o.y]);
+  const points = polygon.map((o) => geom.Point.fromArray(o));
   const width = 1599.0333251953125;
   const height = 580;
 
   const globalInformations = {
-    "sizes": {
-      "motorcycles": 0,
-      "intersections": 0
+    sizes: {
+      motorcycles: 0,
+      intersections: 0,
     },
-    "performance": {
-      "motorcycles": 0,
-      "intersectionCache": 0
-    }
+    performance: {
+      motorcycles: 0,
+      intersectionCache: 0,
+    },
   };
 
   let start = 0;
@@ -58,21 +56,26 @@ export const handler = (argv: Arguments<Options>): void => {
   let motorcycles;
 
   if (fs.existsSync(motorcycles_filename)) {
-    motorcycles = load(motorcycles_filename)["motorcycles"];
+    motorcycles = load(motorcycles_filename)["motorcycles"].map((o) =>
+      mc.MotorcycleSegment.build(o)
+    );
   } else {
     start = performance.now();
     motorcycles = mc.calculateMotorcycles(points, width, height);
-    globalInformations["performance"]["motorcycles"] = performance.now() - start;
+    globalInformations["sizes"]["motorcycles"] = motorcycles.length;
+    globalInformations["performance"]["motorcycles"] =
+      performance.now() - start;
   }
 
   start = performance.now();
   const intersectionCache = mc.calculateIntersectionCache(motorcycles);
-  globalInformations["performance"]["intersectionCache"] = performance.now() - start;
+  globalInformations["performance"]["intersectionCache"] =
+    performance.now() - start;
 
-  globalInformations["sizes"]["motorcycles"] = motorcycles.length;
-  globalInformations["sizes"]["intersections"] = Object.keys(intersectionCache).length;
+  globalInformations["sizes"]["intersections"] =
+    Object.keys(intersectionCache).length;
 
-  const output = {motorcycles, intersectionCache, globalInformations};
+  const output = { intersectionCache, globalInformations };
   let output_filename;
 
   if (argv.hasOwnProperty("output")) {
